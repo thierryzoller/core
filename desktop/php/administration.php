7e251b540b97a964c9cbcaf38c19edd7dbec07ef
Zoller Thierry <thierry@zoller.lu>
2019-08-09 18:36:02 +0200

    Update administration.php

--- desktop/php/administration.php
+++ desktop/php/administration.php
@@ -1701,10 +1701,13 @@ user::isBan();
 													<div class="col-lg-4 col-xs-6">
 														<select class="form-control configKey" data-l1key="core::branch">
 															<option value="master">{{Stable}}</option>
-															<option value="release">{{Release (Pas de support)}}</option>
+															<option value="">{{Stable}}</option>
+															
+															<!--<option value="release">{{Release (Pas de support)}}</option>
 															<option value="v4-release">{{V4 Release (Pas de support)}}</option>
 															<option value="beta">{{Beta (Pas de support)}}</option>
-															<option value="alpha">{{Alpha (Pas de support)}}</option>
+															<option value="alpha">{{Alpha (Pas de support)}}</option>-->
+															
 														</select>
 													</div>
 													<div class="form-group">
