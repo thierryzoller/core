cf3051489a053ad9c30c41a01b6d7d1408a03458
Zoller Thierry <thierry@zoller.lu>
2019-08-10 12:57:40 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 echo 1] Updating from Github https://github.com/thierryzoller/core/
 cd /var/www/html
-sudo git stash save --keep-index
+sudo git add -A
+#sudo git stash save --keep-index
 sudo git stash
 sudo -u www-data git pull origin V4-release
 sudo chown -R www-data:www-data /var/www/html
