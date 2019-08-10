bcc60c8530d88eb5ee7dc2db7780713fa2a4b5a1
Zoller Thierry <thierry@zoller.lu>
2019-08-10 13:00:57 +0200

    Updating

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
-echo 1] Updating from Github https://github.com/thierryzoller/core/
+echo 1. Updating from Git > /var/www/html/log/gireset.log
 cd /var/www/html
-sudo git add -A
+sudo git add -A > /var/www/html/log/gireset.log
 #sudo git stash save --keep-index
-sudo git stash
-sudo -u www-data git pull origin V4-release
-sudo chown -R www-data:www-data /var/www/html
-echo 4] Finished.
+sudo git stash > /var/www/html/log/gireset.log
+sudo -u www-data git pull origin V4-release > /var/www/html/log/gireset.log
+sudo chown -R www-data:www-data /var/www/html > /var/www/html/log/gireset.log
+echo 2. Finished. > /var/www/html/log/gireset.log
