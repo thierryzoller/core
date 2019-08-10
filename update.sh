5b38ee508b0ad5fb561688a7cd84a4c4b7d63779
Zoller Thierry <thierry@zoller.lu>
2019-08-10 13:07:42 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ sudo git stash > /var/www/html/log/gireset.log
 sudo -u www-data git pull origin V4-release > /var/www/html/log/gireset.log
 sudo chown -R www-data:www-data /var/www/html > /var/www/html/log/gireset.log
 echo 2. Finished. > /var/www/html/log/gireset.log
-echo 3. Reset.
+echo 3. Reset..
