37b8e17011f3118f28364114fea2956ce1212447
Zoller Thierry <thierry@zoller.lu>
2019-08-10 13:34:15 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
-echo 1. Updating from Git > /var/www/html/log/gireset.log
+echo 1. Updating from Git >> /var/www/html/log/gireset.log
 cd /var/www/html
-sudo git add -A > /var/www/html/log/gireset.log
+#sudo git add -A > /var/www/html/log/gireset.log
 #sudo git stash save --keep-index
-sudo git stash > /var/www/html/log/gireset.log
-sudo -u www-data git pull origin V4-release > /var/www/html/log/gireset.log
-sudo chown -R www-data:www-data /var/www/html > /var/www/html/log/gireset.log
-echo 2. Finished. > /var/www/html/log/gireset.log
+sudo git stash >> /var/www/html/log/gireset.log
+sudo -u www-data git pull origin V4-release >> /var/www/html/log/gireset.log
+sudo chown -R www-data:www-data /var/www/html >> /var/www/html/log/gireset.log
+echo 2. Finished. >> /var/www/html/log/gireset.log
 echo 3. Reset..
