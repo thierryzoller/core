b49c0e2718d5134bfcd5fe075c2b4123c04faf5c
Zoller Thierry <thierry@zoller.lu>
2019-08-10 13:52:04 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 dt=$(date '+%d/%m/%Y %H:%M:%S');
-echo  >> /var/www/html/log/gitreset.log
+echo \n >> /var/www/html/log/gitreset.log
 echo Action : "$dt" >> /var/www/html/log/gitreset.log
 echo ==================================== >> /var/www/html/log/gitreset.log
-echo  >> /var/www/html/log/gitreset.log
+echo \n >> /var/www/html/log/gitreset.log
 echo 1. Manually Updating from Git >> /var/www/html/log/gitreset.log
 cd /var/www/html
 #sudo git add -A >> /var/www/html/log/gireset.log
@@ -14,6 +14,6 @@ sudo chown -R www-data:www-data /var/www/html >> /var/www/html/log/gitreset.log
 sudo chmod +x update.sh >> /var/www/html/log/gitreset.log
 sudo chmod +x wifion.sh >> /var/www/html/log/gitreset.log
 sudo chmod +x wifioff.sh >> /var/www/html/log/gitreset.log
-echo  >> /var/www/html/log/gitreset.log
+echo \n >> /var/www/html/log/gitreset.log
 echo 2. Finished. >> /var/www/html/log/gitreset.log
 echo ==================================== >> /var/www/html/log/gitreset.log
