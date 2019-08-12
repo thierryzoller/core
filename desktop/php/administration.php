62bc12bae364e9b4bc1c5a8b5242566e7330eb7f
Zoller Thierry <thierry@zoller.lu>
2019-08-12 22:49:10 +0200

    asd
    asd

--- desktop/php/administration.php
+++ desktop/php/administration.php
@@ -84,7 +84,7 @@ if (!$status){
 	</div>
 </div>					
 
-<legend><i class="fas fa-first-aid"</i> {{Recovery}}</legend>
+<legend><i class="fas fa-first-aid"></i> {{Recovery}}</legend>
 
 <div class="form-group">
     <label class="col-lg-2 col-md-3 col-sm-3 col-xs-6 control-label help" data-help="Manually reset update repositry">{{Manualy Reset GIT}}</label>
