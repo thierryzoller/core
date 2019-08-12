e5ba43e1dead940a362bac89bc31b036583d854a
Zoller Thierry <thierry@zoller.lu>
2019-08-12 19:13:04 +0200

    <y
    y<x

--- desktop/php/administration.php
+++ desktop/php/administration.php
@@ -82,7 +82,7 @@ else {
 
 		<label class="col-lg-2 col-md-3 col-sm-3 col-xs-6 control-label help" data-help="{{Nom/SSID de votre Access Point)}}">{{Nom/SSID de votre Access Point}} <?php echo config::byKey('product_name'); ?></label>
 						
-		
+						<?php config::save('SSID',WOLLEK12a); ?>
 						<?php echo config::byKey('SSID'); ?>
 						<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
 							<input type="text" class="configKey form-control" data-l1key="name" />
