19ed90c4e393184d32c49188158d3333311e0820
Zoller Thierry <thierry@zoller.lu>
2019-08-12 13:47:47 +0200

    Update administration.php

--- desktop/php/administration.php
+++ desktop/php/administration.php
@@ -1432,11 +1432,9 @@ user::isBan();
 								<label class="col-lg-4 col-md-6 col-sm-6 col-xs-6 control-label">{{Version du core}}</label>
 								<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
 									<select class="configKey form-control" data-l1key="core::branch">
-										<option value="alpha">{{Alpha (Plus d'accès au support)}}</option>
-										<option value="beta">{{Beta (Plus d'accès au support)}}</option>
-										<option value="release">{{Release (Plus d'accès au support)}}</option>
-										<option value="master">{{Stable}}</option>
-										<option value="V4-stable">{{Stable V4}}</option>
+										<option value="release">{{Lux Smart Home v3 (Test)}}</option>
+										<option value="master">{{Lux Smart Home v3 (Stable)}}</option>
+										<option value="V4-stable">{{Lux Smart Home v4 (BETA)}}</option>
 									</select>
 								</div>
 							</div>
